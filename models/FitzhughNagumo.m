33de279f63dc179f73733dd132a15bcd9ef40e01
Matthew Aburn <mattja6@gmail.com>
2017-02-08 15:15:43 +1000

    Added a generalized FitzHugh-Nagumo oscillator model.

---
+++ models/FitzhughNagumo.m
@@ -0,0 +1,154 @@
+% FitzhughNagumo Generalized FitzHugh-Nagumo neural oscillator model
+%    V' = d*tau*(-f*V.^3 + e*V.^2 + g*V + alpha*W + gamma.*(Iapp + Inet))
+%    W' = d*(c*V.^2 + b*V - beta*W + a)./tau
+% where
+%    Inet = Kij*S(V-theta)
+%    S(x) = 1./(1+exp(-x./sigma))
+%    
+%
+% Example:
+%   n = 20;                     % number of neurons
+%   sys = FitzhughNagumo(n);    % construct the system struct
+%   gui = bdGUI(sys);           % open the Brain Dynamics GUI
+%
+% Authors
+%   Matthew Aburn
+%   Stewart Heitmann
+%
+% See Also:
+% Sanz-Leon et al. (2015) Mathematical framework for large-scale brain network
+%   modeling in The Virtual Brain. NeuroImage 111: 385--430.
+% FitzHugh (1955) Mathematical models of threshold phenomena in the nerve
+%   membrane. Bull. Math. Biophysics, 17:257--278
+
+
+% Copyright (C) 2017, QIMR Berghofer Medical Research Institute
+% All rights reserved.
+%
+% Redistribution and use in source and binary forms, with or without
+% modification, are permitted provided that the following conditions
+% are met:
+%
+% 1. Redistributions of source code must retain the above copyright
+%    notice, this list of conditions and the following disclaimer.
+% 
+% 2. Redistributions in binary form must reproduce the above copyright
+%    notice, this list of conditions and the following disclaimer in
+%    the documentation and/or other materials provided with the
+%    distribution.
+%
+% THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+% "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+% LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+% FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+% COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+% INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+% BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+% LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+% CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+% LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+% ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+% POSSIBILITY OF SUCH DAMAGE.
+function sys = FitzhughNagumo(n)
+    % Construct the default connection matrix (a chain in this case)
+    Kij = circshift(eye(n),1) + circshift(eye(n),-1);
+    
+    % Construct the system struct
+    sys.odefun = @odefun;               % Handle to our ODE function
+    sys.pardef = {'Kij',Kij;            % ODE parameters {'name',value}
+                  'a',0.056;
+                  'b',0.08;
+                  'c',0.0;
+                  'd',1.0;
+                  'e',0.0;
+                  'f',1./3;
+                  'g',1.0;
+                  'alpha',-1.0;
+                  'beta',0.064;
+                  'gamma',1.0;
+                  'tau',1.0;
+                  'Iapp',0.0;
+                  'sigma',1.0;
+                  'theta',0.0};
+    sys.vardef = {'V',5.0*rand(n,1)-2.5;    % ODE variables {'name',value}
+                  'W',3.0*rand(n,1)-0.5};
+    sys.tspan = [0 1000];               % default time span [begin end]
+              
+    % Specify ODE solvers and default options
+    sys.odesolver = {@ode45,@ode23,@ode113,@odeEuler};  % ODE solvers
+    sys.odeoption = odeset('RelTol',1e-6);              % ODE solver options
+
+    % Include the Latex (Equations) panel in the GUI
+    sys.gui.bdLatexPanel.title = 'Equations'; 
+    sys.gui.bdLatexPanel.latex = {'\textbf{FitzhughNagumo}';
+        '';
+        'Network of generalized FitzHugh-Nagumo oscillators.';
+        '\qquad $\dot V_i = \tau d (-f V_i^3 + e V_i^2 + g V_i + \alpha W_i + \gamma (I_{app} + I_{net}))$';
+        '\qquad $\dot W_i = \frac{d}{\tau}(c V_i^2 + b V_i - \beta W_i + a)$';
+        'where';
+        '\qquad $K_{ij}$ is the connectivity matrix ($n$ x $n$),';
+        '\qquad $a, b, c, d, e, f, g, \alpha, \beta, \gamma, \tau, I_{app}, \sigma$ and $\theta$ are constants,';
+        '\qquad $I_{app}$ is the applied current,';
+        '\qquad $I_{net} = \sum_j K_{ij} S(V_j-\theta)$,';
+        '\qquad $S(V) = 1/(1+\exp(-V/\sigma))$,';
+        '\qquad $i{=}1 \dots n$.';
+        '';
+        'Notes';
+        ['\qquad 1. This simulation has $n{=}',num2str(n),'$.'];
+        '';
+        'References:';
+        '\quad Sanz-Leon et al. (2015) Mathematical framework for large-scale brain network modeling in The Virtual Brain, \textit{NeuroImage} \textbf{111}:385--430.';
+        '\quad FitzHugh (1955) Mathematical models of threshold phenomena in the nerve membrane, \textit{Bull. Math. Biophysics}, \textbf{17}:257--278'};
+    
+    % Include the Time Portrait panel in the GUI
+    sys.gui.bdTimePortrait.title = 'Time Portrait';
+ 
+    % Include the Phase Portrait panel in the GUI
+    sys.gui.bdPhasePortrait.title = 'Phase Portrait';
+
+    % Include the Space-Time Portrait panel in the GUI
+    sys.gui.bdSpaceTimePortrait.title = 'Space-Time';
+
+    sys.gui.bdCorrelationPanel = [];
+    
+    % Include the Solver panel in the GUI
+    sys.gui.bdSolverPanel.title = 'Solver';                 
+    
+    % Function hook for the GUI System-New menu
+    sys.self = @self;
+end
+
+% The ODE function for the generalized FitzHugh-Nagumo model.
+function dY = odefun(t,Y,Kij,a,b,c,d,e,f,g,alpha,beta,gamma,tau,I,sigma,theta)  
+    % extract incoming variables from Y
+    Y = reshape(Y,[],2);        % reshape Y to an (nx2) matrix 
+    V = Y(:,1);                 % V is (nx1) vector
+    W = Y(:,2);                 % W is (nx1) vector
+
+    % The network coupling term
+    Inet = Kij*F((V-theta)./sigma);
+    
+    dV = d*tau*(-f*V.^3 + e*V.^2 + g*V + alpha*W + gamma.*(I + Inet));
+    dW = d*(c*V.^2 + b*V - beta*W + a)./tau;
+
+    % return result (2n x 1)
+    dY = [dV; dW];
+end
+
+% Sigmoid function
+function y=F(x)
+    y = 1./(1+exp(-x));
+end
+
+% This function is called by the GUI System-New menu
+function sys = self()
+    % open a dialog box prompting the user for the value of n
+    n = bdEditScalars({100,'number of neurons'}, ...
+        'New System', 'FitzHugh-Nagumo model');
+    % if the user cancelled then...
+    if isempty(n)
+        sys = [];                       % return empty sys
+    else
+        sys = FitzhughNagumo(round(n));  % generate a new sys
+    end
+end
