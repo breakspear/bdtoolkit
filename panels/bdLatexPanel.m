658a387e520dab38df1703e3804f00f98fca44fc
Stewart Heitmann <heitmann@ego.id.au>
2018-01-08 21:11:39 +1100

    Exposed the latex strings to the end-user.

--- panels/bdLatexPanel.m
+++ panels/bdLatexPanel.m
@@ -1,74 +1,32 @@
-classdef bdLatexPanel < handle
-    %bdLatexPanel - Brain Dynamics Toolbox panel for Latex equations.
-    %   This class displays latex equations in the graphical user interface
-    %   of the Brain Dynamics Toolbox (bdGUI) using the MATLAB built-in
-    %   latex interpreter. Users never call this class directly. They
-    %   instead instruct the bdGUI application to load the panel by
-    %   specifying options in their model's sys struct. 
-    %
-    %SYS OPTIONS
-    %   sys.panels.bdLatexPanel.title = 'Equations'
-    %   sys.panels.bdLatexPanel.latex = {'latex string','latex string',...}
-    %
-    %AUTHORS
-    %  Stewart Heitmann (2016a,2017a,2017b,2017c)
-
-    % Copyright (C) 2016,2017 QIMR Berghofer Medical Research Institute
-    % All rights reserved.
-    %
-    % Redistribution and use in source and binary forms, with or without
-    % modification, are permitted provided that the following conditions
-    % are met:
-    %
-    % 1. Redistributions of source code must retain the above copyright
-    %    notice, this list of conditions and the following disclaimer.
-    % 
-    % 2. Redistributions in binary form must reproduce the above copyright
-    %    notice, this list of conditions and the following disclaimer in
-    %    the documentation and/or other materials provided with the
-    %    distribution.
-    %
-    % THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-    % "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-    % LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
-    % FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
-    % COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
-    % INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
-    % BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-    % LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-    % CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
-    % LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
-    % ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-    % POSSIBILITY OF SUCH DAMAGE.
-
-    properties (Access=private) 
-        tab             % handle to uitab object
+classdef bdLatexPanel < bdPanel
+    %UNTITLED Summary of this class goes here
+    %   Detailed explanation goes here
+    
+    properties
+        latex       % cell array of latex strings
     end
-
-    methods        
+    
+    methods
+        
         function this = bdLatexPanel(tabgroup,control)
-            % Construct a new tab panel in the parent tabgroup.
-            % Usage:
-            %    bdLatexPanel(tabgroup,control)
-            % where 
-            %    tabgroup is a handle to the parent uitabgroup object.
-            %    control is a handle to the GUI control object.
-
+            % initialise the base class (specifically this.tab and this.menu)
+            this@bdPanel(tabgroup);
+            
             % apply default settings to sys.panels.bdLatexPanel
             control.sys.panels.bdLatexPanel = bdLatexPanel.syscheck(control.sys);
+
+            % customise the menu
+            this.menu.Label = control.sys.panels.bdLatexPanel.title;
+            uimenu('Parent',this.menu, 'Label','Close', 'Callback',@(~,~) close(this)); 
+
+            % customise the tab
+            this.tab.Title = control.sys.panels.bdLatexPanel.title;
             
             % get the latex string from the sys structure
-            latex = control.sys.panels.bdLatexPanel.latex;
-
-            % construct the uitab
-            this.tab = uitab(tabgroup, ...
-                'title',control.sys.panels.bdLatexPanel.title, ...
-                'Tag','bdLatexPanelTab', ...
-                'Units','points', ...
-                'TooltipString','Right click for menu');
+            this.latex = control.sys.panels.bdLatexPanel.latex;
 
             % construct scrolling uipanel
-            panelh = numel(latex)*32 + 32;      % only approximate (exact height depends on font:pixel ratio)
+            panelh = numel(this.latex)*32 + 32;      % only approximate (exact height depends on font:pixel ratio)
             scrollpanel = bdScroll(this.tab,900,panelh,'BackgroundColor',[1 1 1]); 
 
             % get panel height
@@ -89,16 +47,16 @@ classdef bdLatexPanel < handle
             % monumental strings, and (ii) it is difficult for the user
             % to locate latex syntax errors in monumental strings.
             yoffset = 8;   % points
-            for l = 1:numel(latex)
+            for l = 1:numel(this.latex)
                 
                 % special case: small skip for empty strings
-                if numel(latex{l})==0
+                if numel(this.latex{l})==0
                     yoffset = yoffset + 8;      % small skip
                     continue;
                 end 
                 
                 % render the text
-                obj = text(8,parenth-yoffset, latex{l}, ...
+                obj = text(8,parenth-yoffset, this.latex{l}, ...
                     'interpreter','latex', ...
                     'Parent',ax, ...
                     'Units','pixels', ...
@@ -120,20 +78,17 @@ classdef bdLatexPanel < handle
                 
             end
             
-            % construct the tab context menu
-            this.tab.UIContextMenu = uicontextmenu;
-            uimenu(this.tab.UIContextMenu,'Label','Close', 'Callback',@(~,~) this.delete());
         end
         
-        % Destructor
-        function delete(this)
-            delete(this.tab);          
+        function render(this,control)
         end
-
+        
     end
+    
+    
+    methods (Static)
         
-    methods (Static)    
-        % Check the sys.panels struct
+        % Apply default values to missing fields in sys.panels.bdLatexPanel
         function syspanel = syscheck(sys)
             % Default panel settings
             syspanel.title = 'Equations';
@@ -157,5 +112,5 @@ classdef bdLatexPanel < handle
             end            
         end   
     end
+    
 end
-
