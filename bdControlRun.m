4790dc3661b5cf6cf9772dade678e43fe500092a
Stewart Heitmann <heitmann@ego.id.au>
2019-06-24 15:30:06 +1000

    Tweaked some comments.

--- bdControlRun.m
+++ bdControlRun.m
@@ -1,6 +1,38 @@
 classdef bdControlRun < handle
-    %UNTITLED Summary of this class goes here
-    %   Detailed explanation goes here
+    %bdControlRun  Custom RUN button for the control panel.
+    %  This class implements the RUN button using a custom Java button.
+    %  It is not intended to be called directly by users.
+    % 
+    %AUTHORS
+    %  Stewart Heitmann (2019a)
+
+    % Copyright (C) 2016-2019 QIMR Berghofer Medical Research Institute
+    % All rights reserved.
+    %
+    % Redistribution and use in source and binary forms, with or without
+    % modification, are permitted provided that the following conditions
+    % are met:
+    %
+    % 1. Redistributions of source code must retain the above copyright
+    %    notice, this list of conditions and the following disclaimer.
+    % 
+    % 2. Redistributions in binary form must reproduce the above copyright
+    %    notice, this list of conditions and the following disclaimer in
+    %    the documentation and/or other materials provided with the
+    %    distribution.
+    %
+    % THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+    % "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+    % LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+    % FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+    % COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+    % INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+    % BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+    % LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+    % CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+    % LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+    % ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+    % POSSIBILITY OF SUCH DAMAGE.
     
     properties (Dependent)
         Enable
@@ -15,7 +47,7 @@ classdef bdControlRun < handle
     
     methods
         function this = bdControlRun(control,parent,position)
-            disp('bdControlRun(control,parent,position)');
+            %disp('bdControlRun(control,parent,position)');
             fontname = parent.FontName;
             fontsize = 12;
             jfont = java.awt.Font(fontname,java.awt.Font.PLAIN,fontsize);
