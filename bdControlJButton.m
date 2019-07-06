9037d8bdf1d2ded4d4e58ec5731aa7799b03a512
Stewart Heitmann <heitmann@ego.id.au>
2019-07-06 17:26:03 +1000

    Moved bdControlRun to bdControlJbutton and use it for both the RAND and RUN buttons.

--- bdControlJButton.m
+++ bdControlJButton.m
@@ -1,6 +1,6 @@
-classdef bdControlRun < handle
-    %bdControlRun  Custom RUN button for the control panel.
-    %  This class implements the RUN button using a custom Java button.
+classdef bdControlJButton < handle
+    %bdControlJButton  Custom Java Button for the control panel.
+    %  This class implements a custom Java button that detects the armed state.
     %  It is not intended to be called directly by users.
     % 
     %AUTHORS
@@ -46,12 +46,12 @@ classdef bdControlRun < handle
     end
     
     methods
-        function this = bdControlRun(control,parent,position)
-            %disp('bdControlRun(control,parent,position)');
+        function this = bdControlJButton(control,parent,position,label,visible)
+            %disp('bdControlJButton(control,parent,position)');
             fontname = parent.FontName;
             fontsize = 12;
             jfont = java.awt.Font(fontname,java.awt.Font.PLAIN,fontsize);
-            this.jbutton = javax.swing.JButton('RUN');
+            this.jbutton = javax.swing.JButton(label);
             this.jbutton.setFont(jfont);
             
             % adjust the margin of the text inside the button
@@ -65,18 +65,18 @@ classdef bdControlRun < handle
             [this.hjbutton,this.hcontainer] = javacomponent(this.jbutton, position, parent);
             set(this.hjbutton,'ToolTipText','Run (evolve) the simulation once more');
             %set(this.hjbutton,'ActionPerformedCallback', @(src,~) notify(control,'recompute'));
-            set(this.hcontainer,'Visible','off');
+            set(this.hcontainer,'Visible',visible);
         end
         
         % Get Enable property
         function val = get.Enable(this)
-            %disp('bdControlRun.get.Enable');
+            %disp('bdControlJButton.get.Enable');
             val = get(this.jbutton,'Enabled');
         end
         
         % Set Enable property
         function set.Enable(this,val)
-            %disp('bdControlRun.set.Enable');
+            %disp('bdControlJButton.set.Enable');
             switch val
                 case 'on'
                     set(this.jbutton,'Enabled',1);
@@ -87,19 +87,19 @@ classdef bdControlRun < handle
         
         % Get Visible property
         function val = get.Visible(this)
-            %disp('bdControlRun.get.Visible');
+            %disp('bdControlJButton.get.Visible');
             val = get(this.hcontainer,'Visible');
         end
         
         % Set Visible property
         function set.Visible(this,val)
-            %disp('bdControlRun.set.Visible');
+            %disp('bdControlJButton.set.Visible');
             set(this.hcontainer,'Visible',val);
         end
         
         % Get Armed property
         function val = isArmed(this)
-            %disp('bdControlRun.isArmed');
+            %disp('bdControlJButton.isArmed');
             val = this.jbutton.getModel().isArmed();
         end        
     end
